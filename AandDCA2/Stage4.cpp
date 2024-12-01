5f8d91753250dddff18cf1311b0af2c4f8a1a663
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2024-12-01 21:43:09 +0000

    changing .good to .getline

--- AandDCA2/Stage4.cpp
+++ AandDCA2/Stage4.cpp
@@ -1,10 +1,11 @@
+/*
 #include <iostream>
 #include <fstream>
 #include <string>
 
 using namespace std;
 
-int main()
+int open()
 {
 	ifstream inFile("C:/Users/liamt/Downloads/data.csv");
 
@@ -17,7 +18,27 @@ int main()
 	string vim;
 	string location;
 
-	while (inFile.good()) {
-		
+	while (inFile.getline()) {
+		getline(inFile, carModel, ',');
+		getline(inFile, make, ',');
+		getline(inFile, year, ',');
+		getline(inFile, vim, ',');
+		getline(inFile, location, '\n');
+
+
+		std::cout << "Model: " << carModel << '\n';
+		std::cout << "Make: " << make << '\n';
+		std::cout << "Year: " << year << '\n';
+		std::cout << "Vim: " << vim << '\n';
+		std::cout << "Dealer Location: " << location << '\n';
+
 	}
+	inFile.close();
+	return 0;
 }
+
+int main() {
+	open();
+	return 0;
+}
+*/
