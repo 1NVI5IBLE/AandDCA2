b3937f1759ce953b08bf6a0c4369d7f34aae115f
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2024-12-01 19:38:14 +0000

    Test Constructor And Size, and Test put method

--- AandDCA2Test/test.cpp
+++ AandDCA2Test/test.cpp
@@ -1,8 +1,29 @@
-#include "pch.h"
-
 #include "C:/Users/liamt/source/repos/AandDCA2/AandDCA2/Treemap.h"
+#include "CppUnitTest.h"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace TestTreeMap
+{
+	TEST_CLASS(TestTreeMap)
+	{
+		TEST_METHOD(TestConstructorAndSize)
+		{
+			Treemap<int, std::string> map;
+			Assert::AreEqual(0, map.size());
+		}
+
+
+		TEST_METHOD(TestPut)
+		{
+			Treemap<int, std::string> map;
+			map.put(1, "One");
+			map.put(2, "Two");
+			map.put(3, "Three");
+			Assert::AreEqual(3, map.size());
+		}
+
+
+		
+	};
 }
