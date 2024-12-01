b5cee2a37d5ace49b2665d25c6477b3f6956fce8
1NVI5IBLE <62433063+1NVI5IBLE@users.noreply.github.com>
2024-12-01 21:33:27 +0000

    Opening file sand creating variables to store data

--- AandDCA2/Stage4.cpp
+++ AandDCA2/Stage4.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	ifstream inFile("C:/Users/liamt/Downloads/data.csv");
 
+	if (!inFile.is_open()) std::cout << "Cant open file" << '\n';
+
+
+	string carModel;
+	string make;
+	string year;
+	string vim;
+	string location;
+
+	while (inFile.good()) {
+		
+	}
 }
